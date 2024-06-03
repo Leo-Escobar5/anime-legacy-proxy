7e98fdab4a213a7e5f137c672d517ae833b36251
Leo-Escobar5 <20000453@alumnos.utleon.edu.mx>
2024-06-03 14:16:16 -0600

    home agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Ajusta la ruta según tu estructura de carpetas
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
